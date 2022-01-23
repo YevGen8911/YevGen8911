2fc9cb4fa119d32157583e4463ea4b607de91da2
YevGen8911 <82178954+YevGen8911@users.noreply.github.com>
2022-01-23 21:46:27 +0300

    Create 23.01.22.py

---
+++ 23.01.22.py
@@ -0,0 +1,41 @@
+#1st ex (min number)
+a=int(input())
+b=int(input())
+print(min(a,b))
+
+#2nd ex (sing (x))
+x=int(input())
+if x>0:
+    print(1)
+elif x<0:
+    print(-1)
+else:
+    print(0)
+    
+#3rd ex (chess tile)
+a=int(input())
+b=int(input())
+
+c=int(input())
+d=int(input())
+if (a+b+c+d)%2==0:
+    print("YES")
+else:
+    print("NO")
+    
+#4th ex 
+year=int(input())
+if year%4==0 and year%100!=0 or year%400==0:
+    print("YES")
+else:
+    print("NO")
+    
+#5th ex (min)
+a=int(input())
+b=int(input())
+c=int(input())
+
+print(min(a, b, c))
+
+#6th ex
+
