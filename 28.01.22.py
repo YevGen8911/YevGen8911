65c68a467eefd3bc3d1219ad9273b0284d4992a6
YevGen8911 <82178954+YevGen8911@users.noreply.github.com>
2022-01-28 23:58:03 +0300

    Update 28.01.22.py

--- 28.01.22.py
+++ 28.01.22.py
@@ -13,3 +13,11 @@ else:
     timem=((a*l)+(a//2)*br2+(a//2)*br1)%60
     print(9+timeh)
     print(0+timem)
+
+#snail route
+h=int(input())
+a=int(input())
+b=int(input())
+import math
+x=math.ceil((h-a)/(a-b))+1
+print(x)
